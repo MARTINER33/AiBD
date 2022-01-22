9de7fdfa9f9cb2199f0526e416e2c49414510e46
martiner33 <mapiatek@solarboat.agh.edu.pl>
2022-01-22 22:25:26 +0100

    poprawka numeracji

--- Laboratorium 13 - Pytest/test/test_app.py
+++
@@ -1,9 +0,0 @@
-from app import polly_A
-import numpy as np
-
-def test_polly_A():
-    wek = np.array([2,2,2,2])
-    got = polly_A(wek)
-    want = np.array([16.,-32.,24.,-8.,1.])
-    assert True ==  (got == want).all()
-
